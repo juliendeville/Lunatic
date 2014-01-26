a41081ff61aca1ee6e14ffd4793fbc878d3f8f3e
hugof <hugo.fremont@supinfo.com>
2014-01-26 00:14:41 -0500

    mort vivants

---
+++ Assets/LoadingScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingScript : MonoBehaviour {
+	private GameObject go;
+	
+	void Awake () {
+		go = this.gameObject;
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameObject.DontDestroyOnLoad( go );
+		Application.LoadLevel("Jeu3");
+	}
+
+	
+	void OnLevelWasLoaded(int level) {
+		Destroy( go );
+	}
+}
